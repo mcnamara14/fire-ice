743b2bcf48e962ac1ac5e59d30574c8757434e5f
Tyler McNamara <mcnamara14@gmail.com>
2018-05-31 10:26:35 -0600

    Complete rendering of initial card data

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -15,7 +15,17 @@ export const Card = (props) => {
   const foundedEra = founded ? founded : 'N/A';
   const allSeats = seats.map((seat, index) => {
     return (
-      <p className="seat"  key={index}>seat: {seat}</p>
+      <p className="seat" key={index}>Seat: {seat}</p>
+    );
+  });
+  const allTitles = titles.map((title, index) => {
+    return (
+      <p className="title" key={index}>Title: {title}</p>
+    );
+  });
+  const allWeapons = ancestralWeapons.map((weapon, index) => {
+    return (
+      <p className="weapon" key={index}>Weapon: {weapon}</p>
     );
   });
 
@@ -23,10 +33,17 @@ export const Card = (props) => {
     <article className="houseCard">
       <h2>{name}</h2>
       <h3>{words}</h3>
-      <h4>founded: {foundedEra}</h4>
+      <h4>Founded: {foundedEra}</h4>
       <div className="seats">
         {allSeats}
       </div>
+      <div className="titles">
+        {allTitles}
+      </div>
+      <div className="weapons">
+        {allWeapons}
+      </div>
+      <p>Coat of Arms: {coatOfArms}</p>
     </article>
-  ) 
-}
+  ); 
+};
